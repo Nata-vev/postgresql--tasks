f78623e6f3dd6bbcd1838a5f288c3f9106b06696
Nata-vev <gnataly2589@gmail.com>
2025-01-06 21:52:05 +0200

    Add colums vacation_days and email

--- workers.sql
+++ workers.sql
@@ -6,3 +6,24 @@ CREATE TABLE workers(
   birthday DATE CHECK (birthday BETWEEN '2007-01-01' AND current_date), 
   salary  NUMERIC CHECK(salary >= 0)
 );
+
+-- @block
+ALTER TABLE workers
+ADD COLUMN vacation_days NUMERIC CHECK(vacation_days > 0),
+ADD COLUMN email varchar(256) CONSTRAINT "email must be unique" UNIQUE;
+
+-- @block
+ALTER TABLE workers
+ALTER COLUMN vacation_days SET DEFAULT NULL;
+
+-- @block
+ALTER TABLE workers
+ALTER COLUMN email SET NOT NULL;
+
+-- @block
+ALTER TABLE workers
+ADD CONSTRAINT "not empty email" CHECK (email != '');
+
+-- @block
+ALTER TABLE workers
+ALTER COLUMN salary SET DEFAULT (500);
